d7c91c3aaf48a652ed6a851ecdbbbb04aa278dbf
f_javier <fjmr.10messi@gmail.com>
2022-06-05 21:54:34 +0200

    main

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,94 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <body>
+    <Navbar :item1="item1" :item2="item2" :item3="item3" :item4="item4" :item5="item5" :item6="item6" :logo="logo" :social1="social1" 
+    :social2="social2" :social3="social3"></Navbar>
+    <section class="event py-5" id="events">
+      <div class="container">
+        <div class="row">
+          <Evento :title="e1_title" :title2="e1_title2" :photo="e1_photo" :general_url="e1_general_url" :tickets_url="e1_tickets_url"
+          :prize="e1_prize" :date="e1_date" :location_name="e1_location_name" :location_url="e1_location_url" ></Evento>
+
+          <Evento :title="e2_title" :title2="e2_title2" :photo="e2_photo" :general_url="e2_general_url" :tickets_url="e2_tickets_url"
+          :prize="e2_prize" :date="e2_date" :location_name="e2_location_name" :location_url="e2_location_url" ></Evento>
+
+          <Evento :title="e3_title" :title2="e3_title2" :photo="e3_photo" :general_url="e3_general_url" :tickets_url="e3_tickets_url"
+          :prize="e3_prize" :date="e3_date" :location_name="e3_location_name" :location_url="e3_location_url" ></Evento>
+        </div>
+      </div>
+    </section>
+  </body>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Evento from './components/evento.vue'
+import Navbar from './components/navbar.vue'
 
+import e1_photo_ from './assets/Puro Latino.jpeg'
+import e2_photo_ from './assets/cartel-semana-santa-sevilla-2022.jpg'
+import e3_photo_ from './assets/feria_sevilla.jpg'
+import logo from './assets/logo.png'
+
+  
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'App',
+    components: {
+    Evento,
+    Navbar
+},
+    data: function() {
+      return {
+        e1_title: 'Puro Latino',
+        e1_title2: 'Festival',
+        e1_photo: e1_photo_,
+        e1_general_url: 'https://purolatino.es/sevilla/',
+        e1_tickets_url: 'https://purolatino.es/sevilla/entradas/',
+        e1_prize: '79 €',
+        e1_date: '01/07/22 - 02/07/22',
+        e1_location_name: 'Estadio Olímpico de La Cartuja',
+        e1_location_url: 'https://goo.gl/maps/RNz2n9qG89YMjcMr5',
+
+        e2_title: 'Semana Santa',
+        e2_title2: '2022',
+        e2_photo: e2_photo_,
+        e2_general_url: "https://www.sevilla.org/fiestas-de-la-ciudad/semana-santa/",
+        e2_prize: 'Free',
+        e2_date: '10/04/22 - 16/04/22',
+        e2_location_name: 'Sevilla Centre',
+        e2_location_url: 'https://goo.gl/maps/htgB2xMuTrbaVJqc8',
+
+        e3_title: 'Feria de Sevilla',
+        e3_title2: '2022',
+        e3_photo: e3_photo_,
+        e3_general_url: 'https://www.sevilla.org/fiestas-de-la-ciudad/feria-de-sevilla',
+        e3_prize: 'Free',
+        e3_date: '01/05/22 - 07/05/22',
+        e3_location_name: 'Between Los Remedios and Tablada',
+        e3_location_url: 'https://goo.gl/maps/HyeFmoidtzKBtyEV8',
+
+        item1: "ABOUT US", 
+        item2: "POINTS OF INTEREST", 
+        item3: "EVENTS", 
+        item4: "ITINERARIES", 
+        item5: "SERVICE TYPES", 
+        item6: "CONTACT US", 
+        logo: logo, 
+        social1: "https://es-la.facebook.com/AyuntamientodeSevilla", 
+        social2: "https://twitter.com/sevillaciudad", 
+        social3: "https://www.instagram.com/ayto_sevilla/"
+      };
+    },
+  };
+
 </script>
 
 <style>
+
+div#evento1{
+width: 348px;
+height: 386px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/evento.vue
@@ -0,0 +1,97 @@
+<template>
+
+    <!-- single Event -->
+          <div class="col-10 mx-auto my-3 col-md-6 col-lg-4">
+            <div class="card event-card">
+              <a :href=url(general_url) target="_blank" rel="noopener noreferrer">
+              <img :src=imgPost() class="card-img-top car-img" alt="">
+              </a>
+              <!-- card body -->
+              <div class="card-body">
+                <div class="event-info d-flex justify-content-between">
+                  <!-- first flex child -->
+                  <div class="event-text text-uppercase">
+                    <h6 class="font-weight-bold">{{this.title}}</h6>
+                    <h6>{{this.title2}}</h6> 
+                  </div>
+                  <!-- second flex child -->
+                  <a :href=url(tickets_url) target="_blank" rel="noopener noreferrer">
+
+                  <h5 class="event-value align-self-center py-2 px-3">
+                    <span class="event-price">{{this.prize}}</span>
+                  </h5>
+                  
+                </a>
+                </div>
+              </div>
+              <!-- end of card -->
+              <div class="card-footer text-capitalize d-flex justify-content-between" >
+                <p><span></span>{{date}}</p>
+                <a :href=url(location_url) target="_blank" rel="noopener noreferrer"><span></span>{{this.location_name}}<img src="../assets/maps.png"></a>
+              </div>
+            </div>
+          </div>
+          <!--end of single Event -->
+</template>
+
+
+<script >
+
+export default {
+    props: {
+        title: String, 
+        title2: String, 
+        photo: Image, 
+        general_url: URL, 
+        tickets_url: URL, 
+        prize: String, 
+        date: String, 
+        location_name: String, 
+        location_url: URL
+        },
+        methods:{
+            imgPost(){
+                return this.photo;
+            },
+            url(url){
+              return url;
+            }
+        }
+}
+</script>
+
+
+<style >
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,700');
+
+:root{
+  --mainOrange:rgba(241, 99, 64, 0.7);
+  --transparentOrange:rgba(241, 99, 64, 0.7);
+  --mainWhite:#FFFFFF;
+  --mainGrey:#9c9c9c;
+  --mainBlack:#000000;
+  --mainGrey:rgb(216, 214, 214);
+  --mainYellow:#e3b505;
+}
+    /* Events */
+.event{
+  background: var(--mainGrey);
+}
+.event-card{
+  border-radius: 0!important;
+}
+.event-img{
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+.event-value{
+  background: var(--mainOrange);
+  color: var(--mainWhite);
+  clip-path: polygon(15% 0,85% 0,100% 100%,0 100%);
+  transition: all 1s linear;
+}
+.event-value:hover{
+  clip-path: polygon(0% 0,100% 0,100% 100%,0 100%);
+}  
+
+</style>

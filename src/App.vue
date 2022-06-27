fe47cd31ab6059d0ec447235771235a69c041492
Miguel Cabello García <miguelcabellogarcia@MBP-de-Miguel.lan>
2022-06-27 19:25:26 +0200

    modificiacion miguel

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,15 @@
       :social2="social2" :social3="social3"></Navbar>
     <section class="event py-5" id="events">
       <div class="container">
+         <!-- section title -->
+        <div class="row mb-5">
+          <div class="col d-flex flex-wrap text-uppercase justify-content-center">
+            <!-- <h1 class="font-weight-bold align-self-center mx-1">most important</h1> -->
+            <!-- <a href="Events.html#setas"> -->
+            <h1 class="section-title--special mx-1">events</h1>
+          <!-- </a> -->
+          </div>
+        </div>
         <div class="row">
           <Evento :title="e1_title" :title2="e1_title2" :photo="e1_photo" :general_url="e1_general_url" :tickets_url="e1_tickets_url"
           :prize="e1_prize" :date="e1_date" :location_name="e1_location_name" :location_url="e1_location_url" ></Evento>
@@ -28,6 +37,15 @@
 
      <section class="pointsOfInterest py-5" id="points_of_interest">
        <div class="container">
+         <!-- section title -->
+        <div class="row mb-5">
+          <div class="col d-flex flex-wrap text-uppercase justify-content-center">
+            <!-- <h1 class="font-weight-bold align-self-center mx-1">most important</h1> -->
+            <!-- <a href="Events.html#setas"> -->
+            <h1 class="section-title--special mx-1">points of interest</h1>
+          <!-- </a> -->
+          </div>
+        </div>
   <div class="row">
     <PointOfInterest :title="pI_title" :photo="pI_image" ></PointOfInterest>
     <div class="col-sm-6 d-flex flex-column justify-content-between">
@@ -44,12 +62,109 @@
  
     </div>
     
+
+
+     
     </div>
     </div>
 
                        
 
     </section>
+
+
+<!-- falta la foto -->
+     
+    <section class="services py-5" id="service_types">
+    <div class="container">
+       <!-- section title -->
+        <div class="row mb-5">
+          <div class="col d-flex flex-wrap text-uppercase justify-content-center">
+            <!-- <h1 class="font-weight-bold align-self-center mx-1">most important</h1> -->
+            <!-- <a href="Events.html#setas"> -->
+            <h1 class="section-title--special mx-1">services</h1>
+          <!-- </a> -->
+          </div>
+        </div>
+       <div class="row">
+         <div class="col-10 mx-auto col-lg-6 services-info my-3">
+        <Service :title1="ser_title1" :title2="ser_title2"></Service>  <!-- no se pone en negrita -->
+        <Service :title1="ser_title1" :title2="ser_title2"></Service>
+        <Service :title1="ser_title1" :title2="ser_title2"></Service>
+        <Service :title1="ser_title1" :title2="ser_title2"></Service>
+        <Service :title1="ser_title1" :title2="ser_title2"></Service>
+        <Service :title1="ser_title1" :title2="ser_title2"></Service>
+        <Service :title1="ser_title1" :title2="ser_title2"></Service>
+        <Service :title1="ser_title1" :title2="ser_title2"></Service>
+
+</div>
+       <!-- services img -->
+        <div class="col-10 mx-auto col-lg-6 services-img align-self-center my-3">
+          <!-- img container -->
+          <a href="Service.html">
+          <div class="img-container">
+             <img :src=imgPostServices() alt="" class="img-fluid services-photo">
+            <a href="Service.html" class="services-link">
+              <i class="fas fa-search"></i>
+            </a>
+          </div>
+        </a>
+        </div>
+ 
+       </div>
+      </div>
+
+
+    </section>
+
+
+<!-- poner video de fondo -->
+
+  <section class="itinearies py-2 d-flex align-items-center" id="itinearies">
+    <!-- video container -->
+ <!-- <div class="video-container">
+      <video class="video-item" autoplay loop muted>
+        <source src="assets/videoBcg.mp4" type="video/mp4">Your browser does not support video tag
+      </video>
+    </div> -->
+    <!-- end of video container -->
+    <!-- video overlay -->
+    <div class="video-overlay"></div>
+    <!-- end of video overlay -->
+    <!-- itineary content -->
+    <div class="container py-5">
+       <!-- section title -->
+        <div class="row mb-5">
+          <div class="col d-flex flex-wrap text-uppercase justify-content-center">
+            <!-- <h1 class="font-weight-bold align-self-center mx-1">most important</h1> -->
+            <!-- <a href="Events.html#setas"> -->
+            <h1 class="section-title--special mx-1">itineraries</h1>
+          <!-- </a> -->
+          </div>
+        </div>
+      <div class="row my-3">
+        <!-- col -->
+         <Itinerary :title1="it_title1" :title2="it_title2"></Itinerary>
+        <!-- end col -->
+        <!-- col -->
+        <Itinerary :title1="it_title1" :title2="it_title2"></Itinerary>
+        <!-- end col -->
+        <!-- col -->
+        <Itinerary :title1="it_title1" :title2="it_title2"></Itinerary>
+        <!-- end col -->
+        <!-- end col -->
+        <!-- col -->
+        <Itinerary :title1="it_title1" :title2="it_title2"></Itinerary>
+        <!-- end col -->
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+
   </body>
   
   </html>
@@ -57,24 +172,45 @@
 
 <script>
 import Evento from './components/evento.vue'
-import PointOfInterest from './components/pointOfInterest.vue' //problematic?
+import PointOfInterest from './components/pointOfInterest.vue'
+import Service from './components/services.vue'
+import Itinerary from './components/itineraries.vue'
 import Navbar from './components/navbar.vue'
 import main_header from './components/main_header.vue'
 
 import e1_photo_ from './assets/Puro Latino.jpeg'
 import e2_photo_ from './assets/cartel-semana-santa-sevilla-2022.jpg'
 import e3_photo_ from './assets/feria_sevilla.jpg'
+import ser_photo_ from './assets/torre del oro.jpeg'
 import logo from './assets/logo.png'
 
   
 export default {
+
+
+        methods:{
+            imgPostServices(){
+                return this.photoServices;
+            },
+            url(url){
+              return url;
+            }
+        },
+
     name: 'App',
     components: {
     Evento,
     Navbar,
     PointOfInterest,
+    Service,
+    Itinerary,
     main_header
 },
+
+ props: {
+        photoServices: Image,  
+        },
+
     data: function() {
       return {
 
@@ -121,7 +257,18 @@ export default {
         social3: "https://www.instagram.com/ayto_sevilla/",
 
         pI_title: "Plaza de España",
-        pI_image: "https://upload.wikimedia.org/wikipedia/commons/b/b6/Plaza_de_España_%28Sevilla%29_-_01.jpg"
+        pI_image: "https://upload.wikimedia.org/wikipedia/commons/b/b6/Plaza_de_España_%28Sevilla%29_-_01.jpg",
+
+        ser_title1: "Seville",
+        ser_title2: "Tour Guide",
+
+        it_title1: "Day",
+        it_title2: "1",
+
+
+        photoServices: ser_photo_
+
+
       };
     },
     created: function(){
@@ -139,9 +286,133 @@ export default {
     }
   };
 
+
+
+
+
+
+
+
+
+
+
+
 </script>
 
 <style>
+
+
+
+
+.section-title--special{
+  background: var(--transparentOrange);
+  color: var(--mainWhite);
+  padding: 0.5rem 2rem;
+  clip-path: polygon(10% 0, 100% 0, 90% 100%,0 100%);
+}
+
+/* Services */
+.services{
+  background: var(--mainGrey);
+}
+.services-item{
+  background: var(--mainWhite);
+  border-radius: 0.5rem;
+  opacity: 0.5;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+}
+.services-item:hover{
+  opacity: 1;
+  transform: scale(1.05);
+}
+.services-icon{
+  font-size: 1.5rem;
+  color: var(--mainYellow);
+  cursor: pointer;
+  transition: 0.5s all ease-in-out;
+}
+.services-icon:hover{
+  color: var(--mainBlue);
+}
+.img-container{
+  background: var(--mainBlue);
+  position: relative;
+  overflow: hidden;
+}
+.services-photo{
+  transition: all 1s ease-in-out;
+}
+.img-container:hover .services-photo{
+ transform: scale(1.3);
+ opacity: 0.5;
+}
+.services-link{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  font-size: 3rem;
+  transform: translate(-50%,-50%);
+  color: var(--mainBlack);
+  opacity: 0;
+  transition:all 1s ease-in-out;
+}
+.services-link:hover{
+  color: var(--mainWhite);
+}
+
+.img-container:hover .services-link{
+  opacity: 1;
+}
+
+
+
+  /* Itineary section */
+
+.itinearies{
+  position: relative;
+  min-height: 85vh;
+  max-width: 95vw;
+  margin: 0 auto;
+  clip-path: polygon(0 10%,100% 0,100% 90%, 0 100%);
+  
+}
+.video- {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -3;
+}
+.video-item{
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+/* .video-overlay{
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(241, 99, 64, 0.7), rgba(241, 99, 64, 0.7)), url(../img/la\ giralda\ fondo.jpg)center/cover fixed no-repeat;
+  z-index: -2;
+} */
+.itineary{
+  background: var(--mainBlack);
+  color: var(--mainWhite);
+  transition: all 0.5s ease-in-out;
+}
+.itineary:hover{
+  background: var(--mainWhite);
+  color: var(--mainBlack);
+  padding-left: 2.2rem!important
+}
+.itineary-icon{
+  font-size: 4rem;
+  color: var(--mainYellow);
+}
 
 div#evento1{
 width: 348px;
---
+++ src/components/itineraries.vue
@@ -0,0 +1,125 @@
+<template>
+
+
+    <!-- single Point Of Interest -->
+
+          <!-- first item -->
+ <!-- col -->
+        <div class="col-8 mx-auto col-lg-6 col-md-4 my-3">
+          <div class="itineary py-5 pl-4">
+           <span class="itineary-icon">
+              <i class="fas fa-cogs"></i>
+            </span>
+            <div class="img-container">
+            <h6 class="text-capitalize itineary-title">{{title1}}</h6>
+            <h5 class="text-uppercase font-weight-bold">{{title2}}</h5>
+            </div>
+          </div>
+        </div>
+        <!-- end col -->
+</template>
+
+
+<script >
+
+export default {
+    props: {
+        title1: String, 
+        title2: String, 
+        },
+        methods:{
+            imgPost(){
+                return this.photo;
+            },
+            url(url){
+              return url;
+            }
+        }
+}
+</script>
+
+
+<style >
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,700');
+
+:root{
+  --mainOrange:rgba(241, 99, 64, 0.7);
+  --transparentOrange:rgba(241, 99, 64, 0.7);
+  --mainWhite:#FFFFFF;
+  --mainGrey:#9c9c9c;
+  --mainBlack:#000000;
+  --mainGrey:rgb(216, 214, 214);
+  --mainYellow:#e3b505;
+}
+ /* itineary_form */
+.itineary_form{
+  background: var(--mainWhite);
+}
+.itineary_form-item{
+  background: var(--mainWhite);
+  border-radius: 0.5rem;
+  opacity: 0.5;
+  cursor: pointer;
+  transition: all 1s ease-in-out;
+}
+.itineary_form-item:hover{
+  opacity: 1;
+  transform: scale(1.05);
+}
+.itineary_form-icon{
+  font-size: 1.5rem;
+  color: var(--mainYellow);
+  cursor: pointer;
+  transition: 0.5s all ease-in-out;
+}
+.itineary_form-icon:hover{
+  color: var(--mainBlue);
+}
+.img-container{
+  background: var(--mainBlue);
+  position: relative;
+  overflow: hidden;
+}
+.itineary_form-photo{
+  transition: all 1s ease-in-out;
+}
+.img-container:hover .itineary_form-photo{
+ transform: scale(1.3);
+ opacity: 0.5;
+}
+.itineary_form-link{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  font-size: 3rem;
+  transform: translate(-50%,-50%);
+  color: var(--mainBlack);
+  opacity: 0;
+  transition:all 1s ease-in-out;
+}
+.itineary_form-link:hover{
+  color: var(--mainWhite);
+}
+
+.img-container:hover .itineary_form-link{
+  opacity: 1;
+}
+
+
+/*TEX OF POINT OF INTEREST*/
+
+.centrado{
+  color: white;
+  text-shadow: black 0.2em 0.2em 0.5em;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  text-align: center;
+  transform: translate(-50%, -50%);
+}
+
+img_text{
+  visibility: hidden;
+}
+
+</style>
